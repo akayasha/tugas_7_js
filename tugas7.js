ea686f6732aa5a5cb593dd090ccaccc7905a27cd
Firman Ismail <akayasha99@gmail.com>
2022-02-05 01:29:24 +0700

    tugas 7 js

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+var tb=[150,160,168,167,188,165,177,156,158,169]
+
+for(var i=0; i<tb.length;i++){
+  console.log(tb[i])
+}
+for (let x of tb){
+  console.log(x);
+}
+// perbedaan untuk for dan for of adalah pada itersi for of kita tidak perlu menuliskan indexingnya jadi
+// for of adlah itersi yang paling aman digunakan dalam iterasi array
